b22b72f2133a15f7aeb9165e7c2229f532249be4
Storm Hamilton <stormhamilton@storms-mac-mini.local>
2016-03-09 19:55:43 -0500

    Post Share app integration

--- BTHomePage/ViewController.h
+++ BTHomePage/ViewController.h
@@ -7,9 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    SLComposeViewController *SLComposer;
+}
 
+- (IBAction)Facebook:(id)sender;
+- (IBAction)Twitter:(id)sender;
+
+- (IBAction)Instagram:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *ImageShift;
+
+- (IBAction)Postbutton:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextView *TextToSend;
 
 @end
 
--- BTHomePage/ViewController.m
+++ BTHomePage/ViewController.m
@@ -23,5 +23,44 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+BOOL Facebookbool = YES;
+bool Twitterbool = NO;
+bool Instabool = NO;
 
+
+- (IBAction)Facebook:(id)sender {
+    Facebookbool = YES;
+    Twitterbool = NO;
+    Instabool = NO;
+    self.ImageShift.image = [UIImage imageNamed:@"Facebook icon.png"];
+
+}
+
+- (IBAction)Twitter:(id)sender {
+    Facebookbool = NO;
+    Twitterbool = YES;
+    Instabool = NO;
+    self.ImageShift.image = [UIImage imageNamed:@"TwitterIcon.jpg"];
+}
+
+
+- (IBAction)Instagram:(id)sender {
+    Facebookbool = NO;
+    Twitterbool = NO;
+    Instabool = YES;
+    self.ImageShift.image = [UIImage imageNamed:@"Instagram logo.png"];
+}
+
+- (IBAction)Postbutton:(id)sender {
+    SLComposer = [[SLComposeViewController alloc] init];
+    if (Facebookbool) {
+        SLComposer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+    }
+    else if (Twitterbool){
+        SLComposer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+    }
+    
+    [SLComposer setInitialText:[NSString stringWithFormat:@"%@",self.TextToSend.text]];
+    [self presentViewController:SLComposer animated:YES completion:NULL];
+}
 @end
